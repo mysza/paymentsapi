a25dc67a6855ec37b3c54f4f9eb64c50ed72ab30
Michał Michałowski <michal.michalowski@adtoma.com>
2018-10-05 01:23:10 +0200

    PaymentParty validation rules test
    Refactoring of the code after tests

--- domain/paymentParty.go
+++ domain/paymentParty.go
@@ -1,29 +1,23 @@
 package domain
 
-type partyBase struct {
+// Account holds the base information about an account
+type Account struct {
 	AccountNumber string `json:"account_number" validate:"required"`
 	BankID        string `json:"bank_id" validate:"required"`
 	BankIDCode    string `json:"bank_id_code" validate:"required"`
 }
 
-type paymentParty struct {
-	partyBase
-	AccountName       string            `json:"account_name" validate:"required"`
-	AccountNumberCode AccountNumberCode `json:"account_number_code" validate:"required,oneof=IBAN BBAN"`
-	Address           string            `json:"address" validate:"required"`
-	Name              string            `json:"name" validate:"required"`
+// PaymentParty is a party taking part in the payment transaction
+type PaymentParty struct {
+	Account
+	AccountName       string `json:"account_name" validate:"required"`
+	AccountNumberCode string `json:"account_number_code" validate:"required,oneof=IBAN BBAN"`
+	Address           string `json:"address" validate:"required"`
+	Name              string `json:"name" validate:"required"`
 }
 
-type paymentPartyExtended struct {
-	paymentParty
+// BeneficiaryPaymentParty is a PaymentParty with additional AccountType field
+type BeneficiaryPaymentParty struct {
+	PaymentParty
 	AccountType int `json:"account_type,omitempty" validate:"isdefault"`
 }
-
-// SponsorParty describes the sponsor of a payment
-type SponsorParty partyBase
-
-// DebtorParty describes the debtor of a payment
-type DebtorParty paymentParty
-
-// BeneficiaryParty describes the beneficiary of a payment
-type BeneficiaryParty paymentPartyExtended
--- domain/paymentParty_test.go
+++ domain/paymentParty_test.go
@@ -6,15 +6,15 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-func TestSponsorPartyFieldsValidationRules(t *testing.T) {
+func TestAccountFieldsValidationRules(t *testing.T) {
 	var scenarios = []struct {
 		description string
-		party       SponsorParty
+		account     Account
 		passed      func(error) bool
 	}{
 		{
 			description: "No error if all fields correct",
-			party: SponsorParty{
+			account: Account{
 				AccountNumber: "56781234",
 				BankID:        "123123",
 				BankIDCode:    "GBDSC",
@@ -23,7 +23,7 @@ func TestSponsorPartyFieldsValidationRules(t *testing.T) {
 		},
 		{
 			description: "Error if AccountNumber missing",
-			party: SponsorParty{
+			account: Account{
 				BankID:     "123123",
 				BankIDCode: "GBDSC",
 			},
@@ -31,7 +31,7 @@ func TestSponsorPartyFieldsValidationRules(t *testing.T) {
 		},
 		{
 			description: "Error if BankID missing",
-			party: SponsorParty{
+			account: Account{
 				AccountNumber: "56781234",
 				BankIDCode:    "GBDSC",
 			},
@@ -39,9 +39,165 @@ func TestSponsorPartyFieldsValidationRules(t *testing.T) {
 		},
 		{
 			description: "Error if BankIDCode missing",
-			party: SponsorParty{
+			account: Account{
 				AccountNumber: "56781234",
 				BankID:        "123123",
+			},
+			passed: func(err error) bool { return err != nil },
+		},
+	}
+	validator := validator.New()
+	for _, scenario := range scenarios {
+		t.Run(scenario.description, func(t *testing.T) {
+			if err := validator.Struct(scenario.account); !scenario.passed(err) {
+				t.Errorf("Validation is defined inproperly: %s", err)
+			}
+		})
+	}
+}
+
+func TestPaymentPartyFieldsValidationRules(t *testing.T) {
+	var scenarios = []struct {
+		description string
+		party       PaymentParty
+		passed      func(error) bool
+	}{
+		{
+			description: "No error if all fields correct",
+			party: PaymentParty{
+				Account: Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountName:       "EJ Brown Black",
+				AccountNumberCode: "IBAN",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+				Name:              "EJ Brown Black",
+			},
+			passed: func(err error) bool { return err == nil },
+		},
+		{
+			description: "Error if AccountName missing",
+			party: PaymentParty{
+				Account: Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountNumberCode: "IBAN",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+				Name:              "EJ Brown Black",
+			},
+			passed: func(err error) bool { return err != nil },
+		},
+		{
+			description: "Error if AccountNumberCode missing",
+			party: PaymentParty{
+				Account: Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountName: "EJ Brown Black",
+				Address:     "10 Debtor Crescent Sourcetown NE1",
+				Name:        "EJ Brown Black",
+			},
+			passed: func(err error) bool { return err != nil },
+		},
+		{
+			description: "Error if AccountNumberCode other than from oneof set",
+			party: PaymentParty{
+				Account: Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountNumberCode: "ICCA",
+				AccountName:       "EJ Brown Black",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+				Name:              "EJ Brown Black",
+			},
+			passed: func(err error) bool { return err != nil },
+		},
+		{
+			description: "Error if Address missing",
+			party: PaymentParty{
+				Account: Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountNumberCode: "IBAN",
+				AccountName:       "EJ Brown Black",
+				Name:              "EJ Brown Black",
+			},
+			passed: func(err error) bool { return err != nil },
+		},
+		{
+			description: "Error if Name missing",
+			party: PaymentParty{
+				Account: Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountNumberCode: "IBAN",
+				AccountName:       "EJ Brown Black",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+			},
+			passed: func(err error) bool { return err != nil },
+		},
+	}
+	validator := validator.New()
+	for _, scenario := range scenarios {
+		t.Run(scenario.description, func(t *testing.T) {
+			if err := validator.Struct(scenario.party); !scenario.passed(err) {
+				t.Errorf("Validation is defined inproperly: %s", err)
+			}
+		})
+	}
+}
+
+func TestBeneficiaryPaymentPartyFieldsValidationRules(t *testing.T) {
+	var scenarios = []struct {
+		description string
+		party       BeneficiaryPaymentParty
+		passed      func(error) bool
+	}{
+		{
+			description: "No error if all fields correct",
+			party: BeneficiaryPaymentParty{
+				PaymentParty: PaymentParty{
+					Account: Account{
+						AccountNumber: "56781234",
+						BankID:        "123123",
+						BankIDCode:    "GBDSC",
+					},
+					AccountName:       "EJ Brown Black",
+					AccountNumberCode: "IBAN",
+					Address:           "10 Debtor Crescent Sourcetown NE1",
+					Name:              "EJ Brown Black",
+				},
+				AccountType: 0,
+			},
+			passed: func(err error) bool { return err == nil },
+		},
+		{
+			description: "Error if AccountType != 0",
+			party: BeneficiaryPaymentParty{
+				PaymentParty: PaymentParty{
+					Account: Account{
+						AccountNumber: "56781234",
+						BankID:        "123123",
+						BankIDCode:    "GBDSC",
+					},
+					AccountName:       "EJ Brown Black",
+					AccountNumberCode: "IBAN",
+					Address:           "10 Debtor Crescent Sourcetown NE1",
+					Name:              "EJ Brown Black",
+				},
+				AccountType: 10,
 			},
 			passed: func(err error) bool { return err != nil },
 		},
