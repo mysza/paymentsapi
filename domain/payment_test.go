347b81e2e90f10d7c599e1808808dbba7925facd
Michał Michałowski <michal.michalowski@adtoma.com>
2018-10-05 00:39:34 +0200

    Payment validation definitions tests - init

---
+++ domain/payment_test.go
@@ -0,0 +1,35 @@
+package domain
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+func TestPaymentFieldsValidationRules(t *testing.T) {
+	var scenarios = []struct {
+		description string
+		payment     Payment
+		passed      func(error) bool
+	}{
+		{
+			description: "No error if all fields correct",
+			payment: Payment{
+				ID:             uuid.New(),
+				OrganisationID: uuid.New(),
+			},
+			passed: func(err error) bool { return err == nil },
+		},
+	}
+	validator := validator.New()
+	for _, scenario := range scenarios {
+		t.Run(scenario.description, func(t *testing.T) {
+			fmt.Printf("Payment: %#v", scenario.payment)
+			if err := validator.Struct(scenario.payment); !scenario.passed(err) {
+				t.Errorf("Validation is defined inproperly: %s", err)
+			}
+		})
+	}
+}
