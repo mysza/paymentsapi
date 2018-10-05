bf35314ca1e519196e5d79e05417060219014088
Michał Michałowski <michal.michalowski@adtoma.com>
2018-10-05 23:31:44 +0200

    Update happy path test

--- service/payments_test.go
+++ service/payments_test.go
@@ -401,3 +401,79 @@ func TestUpdateReturnsErrorIfPaymentWithGiveIDDoesNotExist(t *testing.T) {
 		repo.AssertExpectations(t)
 	})
 }
+
+func TestUpdateReturnsNilOnValidInput(t *testing.T) {
+	payment := &domain.Payment{
+		ID:             utils.NewUUID(),
+		OrganisationID: utils.NewUUID(),
+		Attributes: domain.PaymentAttributes{
+			Beneficiary: domain.BeneficiaryPaymentParty{
+				PaymentParty: domain.PaymentParty{
+					Account: domain.Account{
+						AccountNumber: "56781234",
+						BankID:        "123123",
+						BankIDCode:    "GBDSC",
+					},
+					AccountName:       "EJ Brown Black",
+					AccountNumberCode: "IBAN",
+					Address:           "10 Debtor Crescent Sourcetown NE1",
+					Name:              "EJ Brown Black",
+				},
+				AccountType: 0,
+			},
+			Debtor: domain.PaymentParty{
+				Account: domain.Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountName:       "EJ Brown Black",
+				AccountNumberCode: "IBAN",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+				Name:              "EJ Brown Black",
+			},
+			Sponsor: domain.Account{
+				AccountNumber: "56781234",
+				BankID:        "123123",
+				BankIDCode:    "GBDSC",
+			},
+			ChargesInformation: domain.ChargesInformation{
+				BearerCode:              "SHAR",
+				ReceiverChargesAmount:   "100.12",
+				ReceiverChargesCurrency: "USD",
+				SenderCharges: []domain.Charge{
+					domain.Charge{Currency: "USD", Amount: "5.00"},
+					domain.Charge{Currency: "GBP", Amount: "15.00"},
+				},
+			},
+			FX: domain.FX{
+				ContractReference: "FX123",
+				ExchangeRate:      "2.00",
+				OriginalAmount:    "100.12",
+				OriginalCurrency:  "USD",
+			},
+			ProcessingDate:       time.Now(),
+			Amount:               "100.12",
+			Currency:             "USD",
+			EndToEndReference:    "Some generic string",
+			NumericReference:     "123456",
+			PaymentID:            "123456789012345678",
+			PaymentPurpose:       "Paying for goods/services",
+			PaymentScheme:        "FPS",
+			PaymentType:          "Credit",
+			SchemePaymentType:    "InternetBanking",
+			SchemePaymentSubType: "ImmediatePayment",
+			Reference:            "Payment for Em's piano lessons",
+		},
+	}
+	repo := new(mocks.PaymentsRepository)
+	repo.On("Exists", payment.ID).Return(true)
+	repo.On("Update", payment).Return(nil)
+	ps := NewPaymentsService(repo)
+	assert := assert.New(t)
+	t.Run("PaymentsService Update returns nil if the input was valid", func(t *testing.T) {
+		err := ps.Update(payment)
+		assert.Nil(err)
+		repo.AssertExpectations(t)
+	})
+}
