965bb2cdb1c1c69f2c1556aa24d1a992a9e5c454
Michał Michałowski <michal.michalowski@adtoma.com>
2018-10-05 23:28:40 +0200

    Update method

--- service/payments.go
+++ service/payments.go
@@ -3,6 +3,7 @@ package service
 import (
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/mysza/paymentsapi/domain"
 	"github.com/mysza/paymentsapi/utils"
 	validator "gopkg.in/go-playground/validator.v9"
@@ -15,8 +16,9 @@ type PaymentsRepository interface {
 	Add(*domain.Payment) (string, error)
 	GetAll() ([]*domain.Payment, error)
 	Update(*domain.Payment) error
-	Get(string) (*domain.Payment, error)
-	Delete(string) error
+	Get(*uuid.UUID) (*domain.Payment, error)
+	Delete(*uuid.UUID) error
+	Exists(*uuid.UUID) bool
 }
 
 // PaymentsService implements all use cases of the Payments API.
@@ -51,4 +53,15 @@ func (ps *PaymentsService) Add(payment *domain.Payment) (string, error) {
 // GetAll simply returns all payments from the repository.
 func (ps *PaymentsService) GetAll() ([]*domain.Payment, error) {
 	return ps.repo.GetAll()
+}
+
+// Update updates existing payment.
+func (ps *PaymentsService) Update(payment *domain.Payment) error {
+	if err := payment.Validate(ps.validator); err != nil {
+		return err
+	}
+	if exists := ps.repo.Exists(payment.ID); !exists {
+		return fmt.Errorf("Payment with ID: %v does not exist", payment.ID)
+	}
+	return ps.repo.Update(payment)
 }
--- service/payments_test.go
+++ service/payments_test.go
@@ -17,7 +17,7 @@ func TestAddReturnsErrorOnInvalidInput(t *testing.T) {
 			OrganisationID: utils.NewUUID(),
 		}
 		if _, err := ps.Add(payment); err == nil {
-			t.Error("Payments service didn't return error despite invalid input")
+			t.Error("Payments service didn't return error despite invalid input to Add")
 		}
 	})
 }
@@ -173,7 +173,6 @@ func TestAddReturnsErrorIfIDSet(t *testing.T) {
 }
 
 func TestGetAllReturnsAllPaymentsFromRepo(t *testing.T) {
-	repo := new(mocks.PaymentsRepository)
 	payments := []*domain.Payment{
 		&domain.Payment{
 			ID:             utils.NewUUID(),
@@ -302,6 +301,7 @@ func TestGetAllReturnsAllPaymentsFromRepo(t *testing.T) {
 			},
 		},
 	}
+	repo := new(mocks.PaymentsRepository)
 	repo.On("GetAll").Return(payments, nil)
 	ps := NewPaymentsService(repo)
 	assert := assert.New(t)
@@ -309,6 +309,95 @@ func TestGetAllReturnsAllPaymentsFromRepo(t *testing.T) {
 		retPayments, err := ps.GetAll()
 		assert.Equal(payments, retPayments, "Returned payments should be all in repository")
 		assert.Nil(err)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestUpdateReturnsErrorOnInvalidInput(t *testing.T) {
+	t.Run("PaymentsService Update returns error if invalid input passed", func(t *testing.T) {
+		ps := NewPaymentsService(nil)
+		payment := &domain.Payment{
+			ID:             utils.NewUUID(),
+			OrganisationID: utils.NewUUID(),
+		}
+		if err := ps.Update(payment); err == nil {
+			t.Error("Payments service didn't return error despite invalid input to Update")
+		}
+	})
+}
+
+func TestUpdateReturnsErrorIfPaymentWithGiveIDDoesNotExist(t *testing.T) {
+	paymentID := utils.NewUUID()
+	payment := &domain.Payment{
+		ID:             paymentID,
+		OrganisationID: utils.NewUUID(),
+		Attributes: domain.PaymentAttributes{
+			Beneficiary: domain.BeneficiaryPaymentParty{
+				PaymentParty: domain.PaymentParty{
+					Account: domain.Account{
+						AccountNumber: "56781234",
+						BankID:        "123123",
+						BankIDCode:    "GBDSC",
+					},
+					AccountName:       "EJ Brown Black",
+					AccountNumberCode: "IBAN",
+					Address:           "10 Debtor Crescent Sourcetown NE1",
+					Name:              "EJ Brown Black",
+				},
+				AccountType: 0,
+			},
+			Debtor: domain.PaymentParty{
+				Account: domain.Account{
+					AccountNumber: "56781234",
+					BankID:        "123123",
+					BankIDCode:    "GBDSC",
+				},
+				AccountName:       "EJ Brown Black",
+				AccountNumberCode: "IBAN",
+				Address:           "10 Debtor Crescent Sourcetown NE1",
+				Name:              "EJ Brown Black",
+			},
+			Sponsor: domain.Account{
+				AccountNumber: "56781234",
+				BankID:        "123123",
+				BankIDCode:    "GBDSC",
+			},
+			ChargesInformation: domain.ChargesInformation{
+				BearerCode:              "SHAR",
+				ReceiverChargesAmount:   "100.12",
+				ReceiverChargesCurrency: "USD",
+				SenderCharges: []domain.Charge{
+					domain.Charge{Currency: "USD", Amount: "5.00"},
+					domain.Charge{Currency: "GBP", Amount: "15.00"},
+				},
+			},
+			FX: domain.FX{
+				ContractReference: "FX123",
+				ExchangeRate:      "2.00",
+				OriginalAmount:    "100.12",
+				OriginalCurrency:  "USD",
+			},
+			ProcessingDate:       time.Now(),
+			Amount:               "100.12",
+			Currency:             "USD",
+			EndToEndReference:    "Some generic string",
+			NumericReference:     "123456",
+			PaymentID:            "123456789012345678",
+			PaymentPurpose:       "Paying for goods/services",
+			PaymentScheme:        "FPS",
+			PaymentType:          "Credit",
+			SchemePaymentType:    "InternetBanking",
+			SchemePaymentSubType: "ImmediatePayment",
+			Reference:            "Payment for Em's piano lessons",
+		},
+	}
+	repo := new(mocks.PaymentsRepository)
+	repo.On("Exists", paymentID).Return(false)
+	ps := NewPaymentsService(repo)
+	assert := assert.New(t)
+	t.Run("PaymentsService Update returns error if invalid input passed", func(t *testing.T) {
+		err := ps.Update(payment)
+		assert.Error(err, "Update with non-existing payment shoud return error")
 		repo.AssertExpectations(t)
 	})
 }
